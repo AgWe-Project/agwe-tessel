b68e0a2c2400968e2fe900e22782f30aabd74dbb
ProZachJ <prozachj@gmail.com>
2016-05-02 03:37:31 +0000

    bring css local

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var url = require('url');
 var fs = require('fs');
 
+var router = {
+	"/chart.js": "/node_modules/chart.js/dist/Chart.js",
+	"/bootstrap.min.css": "/node_modules/bootstrap/dist/css/bootstrap.min.css",
+	"/bootstrap-theme.min.css": "/node_modules/bootstrap/dist/css/bootstrap-theme.min.css"
+};
+
 module.exports = function handler (req, res){
 	var servefile = function  (err, data) {
 		if (err) {
@@ -16,8 +22,8 @@ module.exports = function handler (req, res){
 	if (path === '/dashboard.js' || path === '/index.html' || path === "/"){
 		path = (path === '/dashboard.js') ? '/dashboard.js' : "/index.html";
 		fs.readFile(__dirname + path, servefile);
-	}else if (path === "/chart.js") {
-		fs.readFile(__dirname + "/node_modules/chart.js/dist/Chart.js", servefile);
+	}else if (router[path] !== undefined) {
+		fs.readFile(__dirname + router[path], servefile);
 	}else{
 		res.writeHead(404);
 		res.end();
